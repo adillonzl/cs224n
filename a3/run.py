0280ba5f4af5b8af741fe41a5fad4b324b9a9ff8
lei.zhou <lei.zhou1@ibm.com>
2019-02-04 15:52:38 -0500

    implement run

--- a3/run.py
+++ a3/run.py
@@ -45,7 +45,8 @@ def train(parser, train_data, dev_data, output_path, batch_size=1024, n_epochs=1
     ### Please see the following docs for support:
     ###     Adam Optimizer: https://pytorch.org/docs/stable/optim.html
     ###     Cross Entropy Loss: https://pytorch.org/docs/stable/nn.html#crossentropyloss
-
+    optimizer = optim.Adam(params=parser.model.parameters(), lr=lr)
+    loss_func = nn.CrossEntropyLoss()
 
     ### END YOUR CODE
 
@@ -98,7 +99,11 @@ def train_for_epoch(parser, train_data, dev_data, optimizer, loss_func, batch_si
             ###      4) Take step with the optimizer
             ### Please see the following docs for support:
             ###     Optimizer Step: https://pytorch.org/docs/stable/optim.html#optimizer-step
-
+            logits = parser.model.forward(train_x)
+            loss = loss_func(logits, train_y)
+            optimizer.zero_grad()   #clear, initiate
+            loss.backward()
+            optimizer.step()
 
             ### END YOUR CODE
             prog.update(1)
